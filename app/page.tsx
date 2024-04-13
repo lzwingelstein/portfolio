16b4c328a68d59c613dd560a964ee3344bcc2108
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@adevinta.com>
2024-04-14 00:36:18 +0200

    [fix] use only px-* on main element
    And add padding top on SocialLinks

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 export default function Home() {
 
   return (
-    <main className="p-4 md:p-8 xl:p-32 ">
+    <main className="px-4 md:px-8 xl:px-32 ">
       <SocialLinks/>
       <HeroSection/>
       <hr className="w-full text-white"/>
