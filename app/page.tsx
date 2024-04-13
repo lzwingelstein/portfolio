52e4aa2b06710341eac006cb90d07e0774327ed6
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@adevinta.com>
2024-04-13 09:00:55 +0200

    [refactor] Refactor the project structure + create component SocialLinks
    Refactored the project structure by moving the app folder to the root of
    the project.
    Use the tailwind colors theme in the CSS classes.
    Also, created a new component SocialLinks that will be used in the
    header and footer of the page.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import SocialLinks from '../src/components/SocialLinks';
+export default function Home() {
+
+  return (
+    <main className="flex flex-col p-4 md:p-5">
+      <SocialLinks/>
+    </main>
+  );
+}
---
+++ src/components/SocialLinks/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import GithubIcon from '@/assets/images/icon-github.svg';
+import LinkedinIcon from '@/assets/images/icon-linkedin.svg';
+import FrontendMentorIcon from '@/assets/images/icon-frontend-mentor.svg';
+
+export default function SocialLinks() {
+    return (
+        <>
+            <div className="flex flex-col md:flex-row justify-between p-5">
+
+            <p className="text-center font-bold">ludoviczwingelstein</p>
+            <p className='p-3 md:p-0'></p>
+            <div className="flex justify-center space-x-4">
+                <a href="https://github.com/lzwingelstein" target="_blank" rel="noopener noreferrer">
+                <Image src={GithubIcon} alt="Github" width={20} height={20} />
+                </a>
+                <a href="https://linkedin.com/in/lzwingelstein" target="_blank" rel="noopener noreferrer">
+                <Image src={LinkedinIcon} alt="Linkedin" width={20} height={20} />
+                </a>
+                <a href="https://www.frontendmentor.io/profile/lzwingelstein" target="_blank" rel="noopener noreferrer">
+                <Image src={FrontendMentorIcon} alt="Frontend Mentor" width={20} height={20} />
+                </a>
+            </div>
+            </div>
+        </>
+    )
+}
