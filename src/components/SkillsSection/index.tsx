75b8324274e4397a0601449f1aca8fb0563cc3e3
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@adevinta.com>
2024-04-15 17:19:44 +0200

    [feat] Add skills sections

--- src/components/SkillsSection/index.tsx
+++ src/components/SkillsSection/index.tsx
@@ -21,17 +21,23 @@ export default function SkillsSection() {
   const backends: string[] = [
     "Go",
     "Node.js",
-    "Next.js",
-    "Docker",
+    "NestJS",
+    "Python",
     "PostgreSQL",
+  ];
+
+  const devops: string[] = [
+    "Docker",
+    "Kubernetes",
+    "AWS",
+    "Jenkins",
+    "GitlabCI",
     "Datadog",
   ];
 
-  const tools: string[] = ["Git", "Github", "Jenkins", "GitlabCI", "Jira"];
+  const domains: string[] = ["Marketplace", "Logistics", "Payment", "SEO"];
 
-  const domains: string[] = ["Marketplace", "Logisticas", "Payment", "SEO"];
-
-  const practice: string[] = [
+  const practices: string[] = [
     "Micro-services",
     "Hexagonal Architecture",
     "MVC",
@@ -39,6 +45,13 @@ export default function SkillsSection() {
     "TDD",
     "DDD",
     "Scrum",
+  ];
+
+  const softskills: string[] = [
+    "Pragmatism",
+    "Teamwork",
+    "Communication",
+    "Problem-solving",
   ];
 
   const skills: SkillData[] = [
@@ -51,18 +64,26 @@ export default function SkillsSection() {
       description: backends.join(separator),
     },
     {
+      name: "Practices",
+      description: practices.join(separator),
+    },
+    {
+      name: "DevOps",
+      description: devops.join(separator),
+    },
+    {
       name: "Domains",
       description: domains.join(separator),
     },
     {
-      name: "Practice",
-      description: practice.join(separator),
+      name: "Soft Skills",
+      description: softskills.join(separator),
     },
   ];
 
   return (
     <section>
-      <ul className="grid md:grid-cols-2 gap-x-28 gap-y-10 pt-10">
+      <ul className="grid md:grid-cols-2 xl:grid-cols-3 gap-x-28 gap-y-14 pt-10">
         {skills.map((skill: SkillData, index: number) => (
           <li key={index} className="basis-full md:basis-1/2">
             <Skill name={skill.name} description={skill.description} />
