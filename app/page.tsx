eba2bf19f94424a130017348572f2220b414477e
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@adevinta.com>
2024-04-15 17:02:41 +0200

    [feat] Add footer with contcat section

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@ import HeroSection from "@/components/HeroSection";
 import SocialLinks from "../src/components/SocialLinks";
 import "./globals.css";
 import SkillsSection from "@/components/SkillsSection";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
-    <main className="px-4 md:px-8 xl:px-32 relative">
-      <div className="absolute top-0 left-0 right-0 z-50 px-4 md:px-8 xl:px-32">
-        <SocialLinks />
+    <main>
+      <div className="px-4 md:px-8 xl:px-32 relative">
+        <div className="absolute top-0 left-0 right-0 z-50 px-4 md:px-8 xl:px-32">
+          <SocialLinks />
+        </div>
+        <HeroSection />
+        <hr className="w-full text-white" />
+        <SkillsSection></SkillsSection>
       </div>
-      <HeroSection />
-      <hr className="w-full text-white" />
-      <SkillsSection></SkillsSection>
+      <Footer />
     </main>
   );
 }
