2303a9e6a80a599d86220f98c7fd12cc6d5331d0
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@gmail.com>
2024-04-18 01:10:17 +0200

    [revert] Revert snap navigation

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ import Footer from "@/components/Footer";
 import ProjectSection from "@/components/ProjectSection";
 export default function Home() {
   return (
-    <main className="snap-y snap-mandatory h-[100vh] overflow-x-hidden overflow-y-scroll">
+    <main className="h-[100vh] overflow-x-hidden overflow-y-scroll">
       <div className="px-4 md:px-8 xl:px-32 relative">
         <div className="absolute top-0 left-0 right-0 z-50 px-4 md:px-8 xl:px-32">
           <SocialLinks />
         </div>
-        <HeroSection className="snap-start min-h-[100vh]" />
+        <HeroSection className="min-h-[100vh]" />
         {/* <hr className="w-full text-white" /> */}
-        <SkillsSection className="snap-start md:min-h-[100vh]" />
+        <SkillsSection className="md:min-h-[100vh]" />
         {/* <hr className="md:hidden w-full text-white" /> */}
-        <ProjectSection className="snap-start min-h-[100vh]" />
+        <ProjectSection className="min-h-[100vh]" />
       </div>
-      <Footer className="snap-start min-h-[100vh]" />
+      <Footer className="min-h-[100vh]" />
     </main>
   );
 }
