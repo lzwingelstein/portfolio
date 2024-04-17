f7a7135d04f2c9b8963807c7b64cead881d487ae
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@gmail.com>
2024-04-18 01:13:05 +0200

    [refactor] Refactor html css to tw

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden bg-dark-600 text-secondary">
       <body>{children}</body>
     </html>
   );
