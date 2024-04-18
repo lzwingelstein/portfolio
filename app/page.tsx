e1fec2508dab3cb872ec3f1fc051e310d49abd82
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@gmail.com>
2024-04-18 11:02:41 +0200

    [feat] Add snap scroll on md+

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ import Footer from "@/components/Footer";
 import ProjectSection from "@/components/ProjectSection";
 export default function Home() {
   return (
-    <main className="h-[100vh] overflow-x-hidden overflow-y-scroll">
+    <main className="h-[100vh] overflow-x-hidden overflow-y-scroll md:snap-y md:snap-mandatory">
       <div className="px-4 md:px-8 xl:px-32 relative">
         <div className="absolute top-0 left-0 right-0 z-50 px-4 md:px-8 xl:px-32">
           <SocialLinks />
         </div>
-        <HeroSection className="min-h-[100vh]" />
-        {/* <hr className="w-full text-white" /> */}
-        <SkillsSection className="md:min-h-[100vh]" />
-        {/* <hr className="md:hidden w-full text-white" /> */}
-        <ProjectSection className="min-h-[100vh]" />
+        <HeroSection className="md:snap-start min-h-[100vh]" />
+        <hr className="md:hidden w-full text-white" />
+        <SkillsSection className="md:snap-start md:min-h-[100vh]" />
+        <hr className="md:hidden w-full text-white" />
+        <ProjectSection className="md:snap-start min-h-[100vh]" />
       </div>
-      <Footer className="min-h-[100vh]" />
+      <Footer className="md:snap-start min-h-[100vh]" />
     </main>
   );
 }
