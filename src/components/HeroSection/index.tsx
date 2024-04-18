53ef2cf7c98a3d1b54ef9ad84ab59ba3fcb5f20b
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@gmail.com>
2024-04-18 16:03:08 +0200

    [style] change picture and add little circle

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import PhotoDesktop from "../../assets/images/photo-profile-desktop.jpeg";
+import PhotoDesktop from "../../assets/images/next-profile-1270x1556.png";
 import Rings from "../../assets/images/pattern-rings.svg";
 import Circle from "../../assets/images/pattern-circle.svg";
 import ContactLink from "../ContactLink";
@@ -14,68 +14,82 @@ export default function HeroSection({ className }: HeroSectionProps) {
     <div className={`${className} flex flex-col`}>
       <div className="flex flex-col items-center justify-between text-center md:flex-row-reverse md:text-left">
         <div className="flex justify-start md:h-[100vh]">
-          <div className="flex items-end h-fit md:h-[80vh] xl:h-[90vh] bg-dark-500 relative md:right-[-2rem] xl:right-[0rem]">
+          <div className="flex items-end h-[40vh] md:h-[80vh] xl:h-[90vh] bg-dark-500 relative md:right-[-2rem] xl:right-[0rem]">
             {/* Mobile picture */}
-            <div className="md:hidden flex w-full z-30 relative">
+            <div className="md:hidden flex w-full z-30 relative pr-[40px]">
               <Image
-                className="absolute bottom-[0px] right-[-45vw] z-50"
+                className="absolute top-[0px] right-[-40vw] z-50"
                 src={Circle}
-                width={129}
-                height={129}
-                alt="Circle"
+                width={90}
+                height={90}
+                alt="Big circle"
               />
               <Image
                 className="absolute bottom-[0px] right-[-12px] z-50"
                 src={Circle}
                 width={24}
                 height={24}
-                alt="Circle"
+                alt="Little circle"
               />
               <Image
                 className="z-10"
                 src={PhotoDesktop}
                 alt="Photo de profile"
-                width={174}
+                width={140}
                 height={383}
               />
             </div>
             {/* Tablette picture */}
-            <div className="max-md:hidden xl:hidden z-30 relative">
+            <div className="max-md:hidden xl:hidden z-30 relative pr-[50px]">
               <Image
-                className="absolute bottom-[0px] right-[-64px] z-50"
+                className="absolute top-[-100px] right-[-64px] z-50"
                 src={Circle}
-                width={129}
-                height={129}
-                alt="Circle"
+                width={120}
+                height={120}
+                alt="Big circle"
+              />
+              <Image
+                className="absolute bottom-[130px] left-[-24px] z-50"
+                src={Circle}
+                width={48}
+                height={48}
+                alt="Little circle"
               />
               <Image
                 className=" z-10"
                 src={PhotoDesktop}
                 alt="Photo de profile"
-                width={322}
+                width={280}
                 height={600}
               ></Image>
             </div>
             {/* Desktop Picture */}
-            <div className="max-xl:hidden z-30 relative h-fit">
+            <div className="max-xl:hidden z-30 relative h-fit pr-[80px]">
               <Image
-                className="absolute bottom-[80px] left-[-62px] z-50"
+                className="absolute bottom-[0px] right-[-64px] z-50"
                 src={Circle}
-                width={129}
-                height={129}
-                alt="Circle"
+                width={120}
+                height={120}
+                alt="Big circle"
+              />
+              <Image
+                className="absolute top-[-240px] left-[-24px] z-50"
+                src={Circle}
+                width={48}
+                height={48}
+                alt="Little circle"
               />
               <Image
                 className="z-10"
                 src={PhotoDesktop}
                 alt="Photo de profile"
-                width={445}
+                width={360}
                 height={720}
               ></Image>
             </div>
           </div>
         </div>
-        <div className=" pt-16 md:w-3/5 md:pt-16 z-50 md:h-[100vh] md:content-center">
+        <div className=" pt-10 md:w-3/5 md:pt-16 z-50 md:h-[100vh] md:content-center">
           <h1 className="md:w-[120%] relative ">
             <Image
               className="absolute top-[-270px] md:top-[-70px] left-[-230px] z-10"
@@ -95,7 +109,7 @@ export default function HeroSection({ className }: HeroSectionProps) {
             Based in Paris, I’m a front-end developer passionate about building
             web apps that users love.
           </p>
-          <p className="pb-16 md:pb-16"></p>
+          <p className="pb-10 md:pb-16"></p>
           <ContactLink />
           <p className="pb-8 md:pb-16"></p>
         </div>
